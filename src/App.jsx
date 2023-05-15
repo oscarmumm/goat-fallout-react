8910ffd0194d4ffd289070c11d3608bf66e0ad28
Oscar Mumm <oscarmumm@gmail.com>
2023-05-15 16:12:38 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import goatlogo from './assets/img/goat-main.webp'
+import "./App.css";
+import { cuestionario } from "./Questions";
+
+function App() {
+
+    return (
+        <>
+            <div className="linea-animada"></div>
+            <h1>Welcome to the G.O.A.T.!!</h1>
+            <img src={goatlogo} alt="" />
+            <div>
+                {cuestionario.map((el) => (
+                    <p>{el.pregunta}</p>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default App;
