8c33e06acb1991aec5c09aa820eeda834e548e07
Oscar Mumm <oscarmumm@gmail.com>
2023-05-21 15:02:41 -0300

    fixed h1 in home jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <main className="main-welcome-container">
             {/* <h1 className="welcome-msg">G.O.A.T.</h1> */}
             <img className="main-pic" src={goatMainImg} alt="" />
-            <p className="welcome-msg">Bienvenido a la Prueba de Aptitud Ocupacional generalizada de Vault-Tec.</p>
+            <p className="welcome-msg">Bienvenido a la Prueba de Aptitud Ocupacional Generalizada de Vault-Tec.</p>
             <NavLink className="start-quiz-btn" to='/quiz'> [ Comenzar prueba ] </NavLink>
         </main>
     );
